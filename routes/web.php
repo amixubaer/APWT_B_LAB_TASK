e960364ff85e0207dc36b918146c59d75d864f1a
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-06-23 13:09:57 +0600

    registration module

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login','LoginController@index')->name('login.index');
+Route::post('/login','LoginController@verify')->name('login.verify');
+
+Route::get('/registration', 'RegistrationController@index')->name('registration.index');
+Route::post('/registration', 'RegistrationController@verify')->name('registration.verify');
